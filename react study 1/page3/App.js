552af43b601b611bf486cccdf8aa895126512393
izza wildan ridhoni <izzawildan88@gmail.com>
2020-12-02 13:57:07 +0700

    belajar react di progate

---
+++ react study 1/page3/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Gunakan tag <h1> untuk mendisplay "Hello World" */}
+        <h1>Hello World</h1>
+        <p>Ayo belajar React bersama-sama!</p>
+        
+        {/* Gunakan tag <p> untuk mendisplay "Ayo belajar React bersama-sama!" */}
+        
+        
+      </div>
+    );
+  }
+}
+
+export default App;
